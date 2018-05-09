76209e67419f8ddd3bae33c67f4ac0456f605fee
eellaadd <34268015+eellaadd@users.noreply.github.com>
2018-05-09 20:38:50 +0300

    Update Point.hpp

--- Point.hpp
+++ Point.hpp
@@ -1,10 +1,30 @@
 #pragma once
-class Point {
-public:
-	int x;
-	int y;
+#include <iostream>
+using namespace std;
 
-	Point(int x, int y) :
-		x(x), y(y) {
-	}
+class Point{
+    public:
+    int x;
+    int y;
+    char data;
+    
+    Point(int x,int y):
+    x(x),y(y){
+    }
+    
+    friend ostream& operator<<(ostream& os, Point& p) {  
+         os << p.x << "," << p.y << endl;
+    return os;  
+    }
+    
+    Point &Point::operator=(const char ch){
+        
+        if (ch == '.' || ch == 'X' || ch == 'O')
+            data = ch;
+        else {
+            IllegalCharException ex {ch};
+            throw ex;
+        }
+        return *this;
+    }
 };
