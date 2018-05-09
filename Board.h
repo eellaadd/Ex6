db9be9115555cc62eee2cfd0b9ee556bb08c52a8
eellaadd <34268015+eellaadd@users.noreply.github.com>
2018-05-09 20:30:20 +0300

    Update and rename Board.hpp to Board.h

---
+++ Board.h
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "Point.h"
+using namespace std;
+
+class Board {
+    public:
+    int size;
+    char* pBoard;
+  //  int x,y;
+  char temp;
+    
+    Board(int size):size(size),
+    pBoard(new char [size*size]){
+        operator=('.');
+    }
+    
+    Board(Board& b){
+        size = b.size;
+        pBoard = new char[size*size];
+        *this = b;
+    }
+    
+    Board& operator=(char w);
+    Board& operator=(Board& b);
+    friend ostream& operator<<(ostream& os, Board& b);
+    int getIndex(int x,int y);
+    char& operator[](Point p);
+};
