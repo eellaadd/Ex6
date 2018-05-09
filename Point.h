bc1be603c4d68cb8a1a48cf741f5cf3ecdd55bcf
eellaadd <34268015+eellaadd@users.noreply.github.com>
2018-05-10 00:29:53 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -3,36 +3,40 @@
 #include "IllegalCharException.h"
 using namespace std;
 
-class Point{
-    public:
-    int x;
-    int y;
-    char data;
-    
-    Point(){
-        data = '.';
-    }
-    
-    Point(int x,int y){
-        this->x = x;
-        this->y = y;
-        this->data = '.';
-    }
-    
-    Point(const Point & p)
-    {
-        this->x = p.x;
-        this->y = p.y;
-        this->data = p.data;
-    }
-    
-    Point operator=(const char c);
-    
-    friend ostream& operator<<(ostream& os, Point& p) {  
-        os << p.data;
-        return os; 
-    }
-    operator char();
+class Point {
+public:
+	int x;
+	int y;
+	char data;
+
+	Point() {
+		data = '.';
+	}
+
+	Point(char c) {
+		this->data = c;
+	}
+
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+		this->data = '.';
+	}
+
+	Point(const Point & p)
+	{
+		this->x = p.x;
+		this->y = p.y;
+		this->data = p.data;
+	}
+
+	Point operator=(const char c);
+
+	friend ostream& operator<<(ostream& os, Point& p) {
+		os << p.data;
+		return os;
+	}
+	operator char();
 };
 
-bool operator == (Point const &, char const &);
+bool operator == (Point const &, char const &)
