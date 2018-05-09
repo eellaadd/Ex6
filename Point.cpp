312c03a49f436f922640f4573c09a46bfb26e7b2
eellaadd <34268015+eellaadd@users.noreply.github.com>
2018-05-10 00:36:50 +0300

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,23 +1,38 @@
-#include "Board.hpp"
-#include "Point.hpp"
+#include "Board.h"
+#include "Point.h"
 #include <iostream>
 using namespace std;
 
-Point Point::operator=(const char c) {
-
-	if (c == '.' || c == 'X' || c == 'O')
-		data = c;
-	else {
-		IllegalCharException ex{ c };
-		throw ex;
-	}
-	return *this;
+Point Point::operator=(const char c){
+    
+    if (c == '.' || c == 'X' || c == 'O')
+        data = c;
+    else {
+        IllegalCharException ex {c};
+        throw ex;
+    }
+    return *this;
 }
 
-Point::operator char() {
-	return data;
+Point& Point::operator=(const Point& p){
+    data = p.data;
+    return *this;
 }
 
-bool operator == (Point const & l, char const & r) {
-	return l.data == r;
+Point::operator char(){
+    return data;
+}
+
+// bool Point::operator==(const char c){
+//     if(data == c){
+//         return data == c;
+//     }
+//     else{
+//         throw 0;
+//     }
+// }
+// Point::~Point(){}
+
+bool operator == (Point const & l, char const & r){
+    return l.data == r;
 }
