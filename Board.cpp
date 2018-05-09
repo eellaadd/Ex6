655222acf397e277b392047a7736509608ccc61f
eellaadd <34268015+eellaadd@users.noreply.github.com>
2018-05-09 23:40:22 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,12 +1,15 @@
 #include "Board.h"
+#include "Point.h"
 #include <iostream>
 using namespace std;
 
 
-Board& Board::operator=(char w){
+Board& Board::operator=(const char w){
     if(w == '.'|| w == 'O' || w == 'X'){
-        for (int i = 0; i < size*size; i++) {
-            pBoard[i]=w;
+        for (int i = 0; i < size; i++) {
+            for(int j = 0; j < size; j++){
+                pBoard[i][j].data = w;
+            }
         }
     }
     else{
@@ -15,38 +18,59 @@ Board& Board::operator=(char w){
     return *this;
 }
 
-Board& Board::operator=(Board& b){
-    
-    for (int i = 0; i < size*size; i++) {
-        pBoard[i]=b.pBoard[i];
+Board& Board::operator=(const Board& b){
+    for (int i = 0; i < size; i++) {
+        for(int j = 0; j < size; j++){
+            pBoard[i][j].data=b.pBoard[i][j].data;
+        }
     }
     return *this;
 }
 
-ostream& operator<<(ostream& os, Board& b) {  
+
+ostream& operator<<(ostream& os, Board const& b) {  
       for (int i = 0; i < b.size; i++) {
         for(int j=0; j<b.size; j++){
-            int index = b.getIndex(i,j);
-            os << b.pBoard[index];
+            os << b.pBoard[i][j].data;
         }
         os << endl;
       }
     return os;  
 }
 
-int Board::getIndex(int x,int y){
-    int index=size*y+x;
-    return index;
-}
-
-//board1[{0,1}] = 'x';
-
 Point& Board::operator[](Point p){
     if(p.x < 0 || p.x >= size || p.y < 0 || p.y >= size){
         throw IllegalCoordinateException(p.x, p.y);
     }
     else{
-        int index=getIndex(p.x,p.y);
-        return p;
+        return pBoard[p.x][p.y];
     }
+
+}
+
+const Point& Board::operator[](Point p) const{
+    if(p.x < 0 || p.x >= size || p.y < 0 || p.y >= size){
+        throw IllegalCoordinateException(p.x, p.y);
+    }
+    else{
+        return pBoard[p.x][p.y];
+    }
+
+}
+
+void Board::free(){
+    for(int i = 0; i < size; i++)
+        delete[] pBoard[i];
+    delete[] pBoard;
+}
+
+// void Board::free(){
+//     for(int i = 0; i < size; i++){
+//         delete[] pBoard[i];
+//     }
+//     delete[] pBoard;
+// }
+
+Board::~Board(){
+	free();
 }
